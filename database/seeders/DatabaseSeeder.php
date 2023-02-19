c8a11c3998d51ef00c49e1a9f4609534a1834ae4
chiaramery <meringolachiara@gmail.com>
2023-02-19 10:21:49 +0100

    Add: PromotionSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([ServicesSeeder::class]);
+        $this->call([PromotionSeeder::class]);
     }
 }
--- database/seeders/PromotionSeeder.php
+++ database/seeders/PromotionSeeder.php
@@ -17,17 +17,28 @@ class PromotionSeeder extends Seeder
     public function run()
     {
         $promotions = [
-            'Wifi',
-            'Posto Macchina',
-            'Piscina',
-            'Portineria',
-            'Sauna',
-            'Vista Mare'
+            [
+                'name' => 'sponsorizzazione di un giorno',
+                'price' => 2.99,
+                'time' => 1
+            ],
+            [
+                'name' => 'sponsorizzazione di tre giorni',
+                'price' => 5.99,
+                'time' => 3
+            ],
+            [
+                'name' => 'sponsorizzazione di sei giorni',
+                'price' => 9.99,
+                'time' => 6
+            ],
         ];
         foreach ($promotions as $promotion) {
             $new_promotion = new Promotion();
-            $new_promotion->name = $promotion;
-            $new_promotion->slug = Str::slug($new_promotion->name, '-');
+            $new_promotion->name = $promotion['name'];
+            $new_promotion->price = $promotion['price'];
+            $new_promotion->time = $promotion['time'];
+            // $new_promotion->slug = Str::slug($new_promotion->name, '-');
             $new_promotion->save();
         }
     }
