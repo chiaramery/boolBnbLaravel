8d23d5b81a5a3a88efe96fb7adddc07dbfc1765d
Serenajandra <serena.jandra@gmail.com>
2023-02-21 16:22:43 +0100

    added style to login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 @section('content')
-    <div class="jumbotron p-5 mb-4  rounded-3 bg-primary ">
+    <div class="jumbotron p-5 mt-5 rounded-3" id="login-jumbo">
         <div class="container-bool py-5">
-            <h1 class="display-5 fw-100 text-center fw-bold">
-                Benvenuti in BoolBnb
-                <p class="fw-bold">
-                    l'app di prenotazione più veloce!
-                </p>
+            <h1 id="welcome-owners" class="display-5 fw-100 text-center fw-bold">
+                Benvenuto
+                <span>
+                    <h5>nell'area di</h5>
+                </span>
+                amministrazione
             </h1>
+
         </div>
     </div>
 @endsection
